5a2df87bc2e352b90e60bb8e9528a942f83f9425
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2021-02-19 10:53:00 -0300

    Adjusted button font, text size and added animations via Motion.

--- src/Components/BaseButton.tsx
+++ src/Components/BaseButton.tsx
@@ -1,84 +1,80 @@
 import React, { useState, useEffect } from 'react';
-import { styled } from '../stitches.config';
+import { motion } from 'framer-motion';
+import { styled, css } from '../stitches.config';
 
 // Styles
-var Button = styled('button', {
-    alignItems: 'center',
-    background: '$vividblue',
-    border: 'none',
-    borderRadius: '4px',
-    color: '#ffffff',
-    cursor: 'pointer',
-    display: 'inline-flex',
-    fontFamily: '$paragraphFont',
-    fontSize: '1.35vh',
-    fontWeight: 500,
-    justifyContent: 'center',
-    letterSpacing: '0.2px',
-    lineHeight: '2.375rem',
-    padding: '0 1rem',
-    textAlign: 'center',
-    textDecoration: 'none',
-    transition: '.07s linear',
-    userSelect: 'none',
-    verticalAlign: 'middle',
+var Button = styled(motion.button, {
+	alignItems: 'center',
+	background: '$vividblue',
+	border: 'none',
+	borderRadius: '4px',
+	color: '#ffffff',
+	cursor: 'pointer',
+	display: 'inline-flex',
+	fontFamily: '$inter',
+	fontSize: '.675vw',
+	fontWeight: 500,
+	justifyContent: 'center',
+	letterSpacing: '0.2px',
+	lineHeight: '2.375rem',
+	padding: '0 1rem',
+	textAlign: 'center',
+	textDecoration: 'none',
+	transition: '.07s linear',
+	userSelect: 'none',
+	verticalAlign: 'middle',
 
-    '&:hover': {
-        
-    },
-    '&:focus': {
-        border: 'none',
-        outline: 'none',
-    },
-    '&:active': {
-        transform: 'scale(0.975)', 
-    },
+	'&:hover': {
+		
+	},
+	'&:focus': {
+		border: 'none',
+		outline: 'none',
+	},
 
-    variants: {
-        mode: {
-            disabled: {
-                background: '$disabledgray',
-                color: '$black',
-                cursor: 'default',
-                '&:hover': {
-                    boxShadow: 'none'
-                },
-                '&:focus': {
-                    
-                },
-                '&:active': {
-                    boxShadow: 'none',
-                }
-            }
-        }
-    }
+	variants: {
+		mode: {
+			disabled: {
+				background: '$disabledgray',
+				color: '$black',
+				cursor: 'default',
+				'&:hover': {
+					boxShadow: 'none'
+				},
+				'&:focus': {
+					
+				},
+			}
+		}
+	}
 });
 
 // Component
 interface IBaseButtonProps {
-    disabled?: boolean,
-    id?: string,
-    isLoading?: boolean,
-    label?: string,
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void,
+	disabled?: boolean,
+	id?: string,
+	isLoading?: boolean,
+	label?: string,
+	onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void,
 };
 
 export const BaseButton: React.FC<IBaseButtonProps> = props => {
-    const buttonRef: React.RefObject<HTMLButtonElement> = React.createRef();
+	const buttonRef: React.RefObject<HTMLButtonElement> = React.createRef();
 
-    function isDisabled() {
-        var { disabled, isLoading } = props;
-        return disabled || isLoading;
-    }
+	function isDisabled() {
+		var { disabled, isLoading } = props;
+		return disabled || isLoading;
+	}
 
-    return (
-        <Button
-        id={props.id}
-        disabled={isDisabled()}
-        mode={isDisabled() ? "disabled" : undefined}
-        onClick={props.onClick}
-        ref={buttonRef}>
-            {props.label || props.children}
-        </Button>
-    );
+	return (
+		<Button
+		id={props.id}
+		disabled={isDisabled()}
+		mode={isDisabled() ? "disabled" : undefined}
+		onClick={props.onClick}
+		whileTap={{ scale: .95 }}
+		ref={buttonRef}>
+			{props.label || props.children}
+		</Button>
+	);
 }
