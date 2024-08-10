821160bd1c334c5830dd168229f79964e0751685
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-08-09 22:15:13 -0300

    Updated resume file.

--- src/data/socials.tsx
+++ src/data/socials.tsx
@@ -2,5 +2,5 @@ export const Socials = {
 	Github: 'https://github.com/gspalato',
 	Instagram: 'https://instagram.com/spxlato/',
 	LinkedIn: 'https://www.linkedin.com/in/spalato/',
-	Resume: 'https://docs.google.com/document/d/1nBt1Dij2soL-FvkHA5BaADoxopGyFusZpZ5MQXoPgsI/edit?usp=sharing',
+	Resume: 'https://docs.google.com/document/d/1FALDckvOBjGuFRCmm6zLcdBHRoazA3GcIr45AdsqLxs/edit?usp=sharing',
 } as const;
